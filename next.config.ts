96904bdf17a8aa1b2b4a24dfddbe5d5b09f4b462
Rahul Nair <nair.rahul992@gmail.com>
2025-10-06 22:06:11 -0700

    img fix

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export'
+  output: 'export',
+  basePath: '/name-generator-utils'
 };
 
 export default nextConfig;
